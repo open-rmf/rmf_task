efca418fc42c3ea396cf95ebf4d3cfbb88943c08
Michael X. Grey <mxgrey@intrinsic.ai>
2024-03-08 10:00:27 +0000

    Introduce some debug output
    Signed-off-by: Michael X. Grey <mxgrey@intrinsic.ai>

--- rmf_task_sequence/src/rmf_task_sequence/Task.cpp
+++ rmf_task_sequence/src/rmf_task_sequence/Task.cpp
@@ -245,6 +245,25 @@ private:
 
   Backup _empty_backup() const;
 
+  std::string _task_id() const
+  {
+    if (_tag)
+    {
+      if (const auto booking = _tag->booking())
+      {
+        return booking->id();
+      }
+      else
+      {
+        return "<booking missing>";
+      }
+    }
+    else
+    {
+      return "<tag missing>";
+    }
+  }
+
   Active(
     Phase::ConstActivatorPtr phase_activator,
     std::function<rmf_traffic::Time()> clock,
@@ -785,15 +804,29 @@ void Task::Active::_generate_pending_phases()
 //==============================================================================
 void Task::Active::_finish_phase(Phase::Tag::Id id)
 {
+  std::cout << "Triggering finish for phase " << id
+    << " of task [" << _task_id() << "]" << std::endl;
   std::lock_guard<std::recursive_mutex> lock(_next_phase_mutex);
   if (!_active_stage)
+  {
+    std::cerr << "[rmf_task_sequence::Task::Active::_finish_phase] "
+      << "No active stage is present for task [" << _task_id()
+      << "]." << std::endl;
     return;
+  }
 
   if (_active_stage->id != id)
+  {
+    std::cerr << "[rmf_task_sequence::Task::Active::_finish_phase] "
+      << "Mismatch in phase ID of task [" << _task_id() << "]. Expected: " << id
+      << ". Current: " << _active_stage->id << std::endl;
     return;
+  }
 
+  std::cout << "Beginning next phase of task [" << _task_id() << "] after "
+    << id << std::endl;
   _completed_stages.push_back(_active_stage);
-  _active_stage = nullptr;
+  // _active_stage = nullptr;
 
   const auto phase_finish_time = _clock();
   const auto completed_phase = std::make_shared<Phase::Completed>(
@@ -810,6 +843,7 @@ void Task::Active::_finish_phase(Phase::Tag::Id id)
 //==============================================================================
 void Task::Active::_begin_next_stage(std::optional<nlohmann::json> restore)
 {
+  std::lock_guard<std::recursive_mutex> lock(_next_phase_mutex);
   if (_task_is_interrupted)
   {
     // If we currently expect the task to be interrupted but we reach this
@@ -835,7 +869,21 @@ void Task::Active::_begin_next_stage(std::optional<nlohmann::json> restore)
   while (true)
   {
     if (_pending_stages.empty())
+    {
+      std::stringstream ss;
+      ss << "Finishing task [" << _task_id() << "]";
+      if (_active_stage)
+      {
+        ss << " at stage " << _active_stage->id;
+      }
+      else
+      {
+        ss << " with no active stage";
+      }
+
+      std::cout << ss.str() << std::endl;
       return _finish_task();
+    }
 
     bool stage_and_phase_consistency = true;
     if (_pending_stages.size() != _pending_phases.size())
